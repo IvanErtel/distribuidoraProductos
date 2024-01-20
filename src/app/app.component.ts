162360ea1640d00e3bdafcfed3835696e32c53fc
Ivan Ertel <alanertel2275@gmail.com>
2024-01-20 19:54:23 -0300

    primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CategoriasComponent } from './components/categorias/categorias.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, 
+  RouterOutlet, 
+  NavbarComponent,
+  CarouselComponent,
+  CategoriasComponent,
+  ContactoComponent,
+  FooterComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'vineria-angular';
+}
